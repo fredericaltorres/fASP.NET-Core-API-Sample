697046683fae4e0ab150b4f5501d6613dd248a45
Frederic Torres <ftorres@emoneyadvisor.com>
2018-11-09 22:02:19 -0500

    Creation

---
+++ src/CityInfo.API/Controllers/CitiesControllers.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    /// <summary>
+    /// HTTP Status Code
+    /// 200 - Ok
+    /// 201 - Ok created
+    /// 204 - Ok no content (delete)
+    /// 300 - Redirection page moved
+    /// 400 - Bad request
+    /// 401 - Unauthorized
+    /// 403 - Forbidden
+    /// 404 - Not found
+    /// 409 - Conflict
+    /// 500 - Server error - internal error
+    /// </summary>
+    [Route(CitiesController.ROUTE)]
+    public class CitiesController : Controller
+    {
+        public const string ROUTE = "api/cities";
+
+        /// <summary>
+        /// http://localhost:1028/api/cities
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        /// <summary>
+        /// http://localhost:1028/api/cities/1
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if(city == null)
+                return NotFound();
+            return Ok(city);
+        }
+
+        // [HttpGet()]
+        public JsonResult GetJsonCities()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new { id=1, Name="New York City"},
+                    new { id=2, Name="Antwer"},
+                }
+            );
+        }
+    }
+}
---
+++ src/CityInfo.API/Controllers/PointsOfinterestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    /// <summary>
+    /// PointsOfInterest resource is a child resource of cities resource
+    /// </summary>
+    [Route(CitiesController.ROUTE)]
+    public class PointsOfInterestController : Controller
+    {
+        /// <summary>
+        /// http://localhost:1028/api/cities/1/pointsofinterest
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+                return NotFound();
+            return Ok(city.PointsOfInterest);
+        }
+
+        /// <summary>
+        /// http://localhost:1028/api/cities/1/pointsofinterest/1
+        /// </summary>
+        /// <param name="cityId"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{cityId}/pointsofinterest/{id}")]
+        public IActionResult GetPointsOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+                return NotFound();
+            var poi = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if(poi == null)
+                return NotFound();
+            return Ok(poi);
+        }
+    }
+}
