ecad2213fef3531ac997c4d5d344304722627b40
Frederic Torres <ftorres@emoneyadvisor.com>
2018-11-11 18:18:32 -0500

    Added json configuration

--- fASP.NET-Core-API-Sample/Startup.cs
+++ fASP.NET-Core-API-Sample/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
@@ -14,6 +15,16 @@ namespace CityInfo.API
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration;
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(env.ContentRootPath)
+                        .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true);
+
+            Startup.Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
